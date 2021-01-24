d2753ff9e88871d0c63f52529d0069948744585f
Aaron William Po <apo2@uwo.ca>
2021-01-24 09:12:18 -0500

    edited index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const path = require('path');
+const port = 3001;
+
 const app = express();
-const port = 3000;
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongo connection open.'))
+  .catch(err => console.log(err))
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log();
+  console.log(`App is listening on port ${port}.`);
 })

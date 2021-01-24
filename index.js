0543d868722f7a1ccbb9557df4901f524b5a0b4b
Aaron William Po <apo2@uwo.ca>
2021-01-24 09:50:55 -0500

    some edits

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const express = require('express');
 const path = require('path');
 const port = 3001;
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongo connection open.'))
+  .catch(err => console.log(`Mongo connection error.\n${err}`))
+
+const Product = require('./models/product')
+
 const app = express();
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Mongo connection open.'))
-  .catch(err => console.log(err))
-
-
-
-
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
